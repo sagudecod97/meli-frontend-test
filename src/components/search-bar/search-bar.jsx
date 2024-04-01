919c218b9ca0190321e7f857335ba5d0459ce68c
Santiago Agudelo <sagudecod97@gmail.com>
2024-04-01 10:20:50 -0500

    Cree componenet para miga de pan

--- src/components/search-bar/search-bar.jsx
+++ src/components/search-bar/search-bar.jsx
@@ -1,6 +1,6 @@
 import "./search-bar.scss";
 import { useContext, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { SearchContext } from "../../context/search.context";
 
 const SearchBar = () => {
